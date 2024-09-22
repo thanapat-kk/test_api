156042a25c5d0a0a27cfa1dd4bbad9fd239fae42
thanapat1999 <thanapat1999@gmail.com>
2024-09-22 22:47:45 +0700

    Initial commit

---
+++ api_test.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/getcode', methods=['GET'])
+def getcode():
+    # คุณสามารถส่งค่าตัวเลขหรือข้อความตามต้องการ
+    return "12345"  # หรือจะเป็นข้อความอื่นๆ เช่น "Hello, this is your code"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
